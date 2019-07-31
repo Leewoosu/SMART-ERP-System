5059460950079df0bac35b00b7954305c5718fa7
Leewoosu <lus0024@naver.com>
2019-07-31 09:39:20 +0900

    20190731

---
+++ SMART ERP System/SMART ERP System/LoginForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SMART_ERP_System
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbEmployeeCode = new System.Windows.Forms.TextBox();
+            this.txbEmployeeName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbPassWord = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "사원코드";
+            // 
+            // txbEmployeeCode
+            // 
+            this.txbEmployeeCode.Location = new System.Drawing.Point(71, 21);
+            this.txbEmployeeCode.Name = "txbEmployeeCode";
+            this.txbEmployeeCode.Size = new System.Drawing.Size(110, 21);
+            this.txbEmployeeCode.TabIndex = 1;
+            this.txbEmployeeCode.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TxbEmployeeCode_KeyUp);
+            // 
+            // txbEmployeeName
+            // 
+            this.txbEmployeeName.Location = new System.Drawing.Point(71, 53);
+            this.txbEmployeeName.Name = "txbEmployeeName";
+            this.txbEmployeeName.ReadOnly = true;
+            this.txbEmployeeName.Size = new System.Drawing.Size(110, 21);
+            this.txbEmployeeName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "이     름";
+            // 
+            // txbPassWord
+            // 
+            this.txbPassWord.Location = new System.Drawing.Point(71, 84);
+            this.txbPassWord.Name = "txbPassWord";
+            this.txbPassWord.Size = new System.Drawing.Size(110, 21);
+            this.txbPassWord.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "암     호";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(71, 120);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(53, 23);
+            this.btnOk.TabIndex = 6;
+            this.btnOk.Text = "&OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.BtnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(128, 120);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(53, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 154);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txbPassWord);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbEmployeeName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbEmployeeCode);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "LoginForm";
+            this.Text = "로그인";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbEmployeeCode;
+        private System.Windows.Forms.TextBox txbEmployeeName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbPassWord;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
